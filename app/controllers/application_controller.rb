c3421efd2e0738b0e061d9448f7b9f58cb6aede4
Jason Viger <jason.viger@gmail.com>
2018-08-31 09:17:50 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,7 +23,6 @@ class ApplicationController < Sinatra::Base
   get '/account' do
      @user = User.find_by_id(user_id: session[:user_id])
     if @user
-      session[:user_id] = @user.id
       erb :account
     else
       erb :error
